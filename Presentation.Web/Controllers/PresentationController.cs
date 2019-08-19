d96b9824b4297317ec3caeedb7f48019a45f7ee7
s.dremo@npc.pl <s.dremo@npc.pl>
2019-08-19 16:03:58 +0200

    Start of repo

---
+++ Presentation.Web/Controllers/PresentationController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PresentationController : ControllerBase
+    {
+        // [HttpGet("get-presentation")]
+        // public FileResult GetPresentation([FromBody])
+        // {
+
+        // }
+    }
+}
